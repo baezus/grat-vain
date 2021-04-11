531ee2b8afc60922dd91bf3b1e61ad2e24720d91
Elias <elias@baez.us>
2021-04-11 12:28:13 -0400

    Got the handle submit working on the register page, now just linking in axios.

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -52,40 +52,30 @@ const Register = () => {
 
   return (
     <div className="registerForm is-grouped-right" onSubmit={handleSubmit}>
+      <form className="control">
       <div className="field">
         <label className="label">Name</label>
         <div className="control">
           <input className="input" value={data.name} name="name" type="text" placeholder="First Name" onChange={handleNameInputChange} disabled={submitted}/>
         </div>
       </div>
-      {submitted && !data.name && <span id="name-error">Please enter a name.</span>}
+
       <div className="field">
         <label className="label">Display Name</label>
         <div className="control has-icons-left has-icons-right">
           <input className="input is-success" type="text" placeholder="Display Name" name="display" value={data.display} onChange={handleDisplayInputChange}/>
-          <span className="icon is-small is-left">
-            <i className="fas fa-user"></i>
-          </span>
-          <span className="icon is-small is-right">
-            <i className="fas fa-check"></i>
-          </span>
         </div>
       </div>
-      {submitted && !data.display && <span id="display-error">Please enter a display name.</span>}
+
       <div className="field">
         <label className="label">Email</label>
         <div className="control has-icons-left has-icons-right">
           <input className="input is-danger" type="email" placeholder="Email" name="email" value={data.email} onChange={handleEmailInputChange}/>
-          <span className="icon is-small is-left">
-            <i className="fas fa-envelope"></i>
-          </span>
-          <span className="icon is-small is-right">
-            <i className="fas fa-exclamation-triangle"></i>
-          </span>
+
         </div>
         <p className="help is-danger">This email is invalid</p>
       </div>
-      {submitted && !data.email && <span id="email-error">Enter an email address.</span>}
+
       <div className="field">
         <label className="label">Password</label>
         <div className="control">
@@ -95,13 +85,10 @@ const Register = () => {
 
       <div className="field is-grouped">
         <div className="control">
-          <button className="button is-link" onClick={handleSubmit}>Submit</button>
-        </div>
-        <div className="control">
-          <button className="button is-link is-light">Cancel</button>
+          <button className="button is-link" type="submit" onSubmit={handleSubmit}>Submit</button>
         </div>
       </div>
-      {submitted && <div className="success-message">Success! You have registered successfully.</div>}
+      </form>
     </div>
   )
 };
