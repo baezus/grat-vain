d10f9c09bec4a309cebf461ddf9f2f47a9939673
Elias <elias@baez.us>
2021-04-11 09:15:03 -0400

    Added more foundation to the backend.

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,52 @@
-require('dotenv').config();
+//imports
 
+require('dotenv/config');
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+
+//middleware
+
 app.use(cors());
+app.options('*', cors());
+app.use(morgan('tiny'));
+app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
+
+//router imports
+
+const usersRouter = require('./routes/users');
+const poemsRouter = require('./routes/poems');
+
+//router setup
+
+const api = process.env.API_URL;
+app.use(`${api}/users`, usersRouter);
+app.use(`${api}/poems`, poemsRouter);
+
+//listenings
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  dbName: 'vanity'
+})
+.then(() => {
+  console.log('MongoDB connection up and running.');
+})
+.catch((err) => {
+  console.log('ERROR!: ', err);
+});
+
+
 const server = require('http').createServer(app);
 
 //config
 
-const port = 2737;
+const port = process.env.PORT;
 server.listen(port, () => {
   console.log(`Server running at localhost:${port}`);
 });
