ff213ecdc12ac952c97bcbd974d024f0f7dc2307
Elias <elias@baez.us>
2021-04-10 21:07:40 -0400

    Got fairly complex Navbar and hamburger controls all up and running, using NavLink and React router. Getting close to the backend.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,42 @@ import logo from './logo.svg';
 import Splash from './pages/Splash';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './styles/App.scss';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <header className="App-header">
         <Navbar/>
       </header>
       <body>
-        <Splash/>
+        <div className="container" style={{marginTop:40}}>
+          <Switch>
+
+            <Route exact path="/">
+              <Splash/>
+            </Route>
+
+            <Route exact path="/register">
+              <Register/>
+            </Route>
+
+            <Route exact path="/login">
+              <Login/>
+            </Route>
+
+          </Switch>
+        </div>
       </body>
       <div className="footer-div">
         <Footer/>
       </div>
     </div>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,29 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+
+  const [isActive, setIsActive] = useState(false);
+  const [isOpen, setOpen] = useState(false);
 
   return(
     <>
       <nav className="navbar is-transparent is-fixed" role="navigation" aria-label="main-navigation">
-        <div className="navbar-brand">
-          <a className="navbar-item" id="home">
-            Vanity
-          </a>
-        </div>
-        <div className="navbar-menu is-active">
-          <div className="navbar-end">
-            <a className="navbar-item">
-              For Poets 
+        <div className="container">
+          <div className="navbar-brand">
+            <NavLink className="navbar-item" activeClassName="is-active" to="/" exact>
+              Vanity
+            </NavLink>
+            <a 
+              onClick={() => {setOpen(!isOpen)}}
+              role="button"
+              className={`navbar-burger burger ${isOpen && "is-active"}`}
+              aria-label="menu"
+              aria-expanded="false"
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
             </a>
-            <a className="navbar-item">
-              For Editors 
-            </a>
-            <a className="navbar-item">
-              Log In 
-            </a>
-            <a className="navbar-item">
-              Register
-            </a>
+          </div>
+          <div 
+            className={`navbar-menu ${isOpen && "is-active"}`}>
+            <div className="navbar-start">
+              <NavLink className="navbar-item" activeClassName="is-active" to="/poets" exact>
+                For Poets 
+              </NavLink>
+              <NavLink className="navbar-item" activeClassName="is-active" to="/editors" exact>
+                For Editors 
+              </NavLink>
+              <NavLink className="navbar-item" activeClassName="is-active" to="/login" exact>
+                Log In 
+              </NavLink>
+              <NavLink className="navbar-item" activeClassName="is-active" to="/register" exact>
+                Register
+              </NavLink>
+            </div>
           </div>
         </div>
       </nav>
